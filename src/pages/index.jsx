83a75bb09fa37ab6a88caad5b02437144c6b5da7
Jana Šturmová <jana.sturmova@seznam.cz>
2024-06-24 20:59:47 +0200

    komponenty

---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { Menu } from '../components/Menu';
+import { Gallery } from '../components/Gallery';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
+
+document.querySelector('#root').innerHTML = render(
+  <div className="page">
+    <Header/>
+    <main>
+      <Banner/>
+      <Menu/>
+      <Gallery/>
+      <Contact/>      
+    </main>
+    <Footer/>
+  </div>
+);
